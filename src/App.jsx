f463e08da3267d148cc2decd2e206d5ad241a7c4
prajakt <prajaktb159@gmail.com>
2026-07-02 14:59:07 +0530

    E-Store React Redux project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { Routes, Route } from "react-router-dom";
-
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import Products from "./pages/Products";
-
 import "./App.css";
 
 function App() {
